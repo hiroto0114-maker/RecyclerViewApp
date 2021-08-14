c23884eac0b67fdffc9ce9ae410ab6e6a2fe8af5
hiroto-murayama <0me6-90y2k-4f0k@au.com>
2021-08-14 23:44:20 +0900

    this@mainactivityの修正完了

--- app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
@@ -4,10 +4,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import android.widget.Toast as Toast
 
 class MainActivity : AppCompatActivity() {
     lateinit var recyclerView: RecyclerView
@@ -33,28 +33,21 @@ class MainActivity : AppCompatActivity() {
             "フシギダネ"
         )
         recyclerView = findViewById(R.id.recyclerView)
-        recyclerView.adapter = MyRecyclerViewAdapter(pokemonList)
         recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = MyRecyclerViewAdapter(pokemonList)
+        recyclerView.adapter = MyRecyclerViewAdapter(pokemonList)
 
+        adapter.setOnItemClickListener(View.OnClickListener { view ->
+            val pokemonName = view.findViewById<TextView>(R.id.title)
+            val pokemonName1 = pokemonName.text.toString()
+            Toast.makeText(this@MainActivity, pokemonName1, Toast.LENGTH_LONG).show()
+
+        })
+    }
+    interface OnItemClickListener {
+        fun onItemClick(view: View?)
     }
 
-    class MyRecyclerViewAdapter(val list: List<String>) : RecyclerView.Adapter<MyViewHolder>() {
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_recycler_view, parent, false)
-            itemView.setOnClickListener(ItemClickListener())
-            return MyViewHolder(itemView)
-        }
-
-        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-            holder.positionText.text = position.toString()
-            holder.titleText.text = list[position]
-        }
-
-        override fun getItemCount(): Int = list.size
-
-
-    }
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val positionText: TextView
         val titleText: TextView
@@ -63,11 +56,23 @@ class MainActivity : AppCompatActivity() {
             titleText = itemView.findViewById(R.id.title)
         }
     }
-    class ItemClickListener : View.OnClickListener {
-        override fun onClick(view: View){
-            val pokemonName=view.findViewById<TextView>(R.id.title)
-            val pokemonName1=pokemonName.text.toString()
-            Toast.makeText(this@MainActivity, pokemonName1, Toast.LENGTH_SHORT).show()
+    class MyRecyclerViewAdapter(val list: List<String>) : RecyclerView.Adapter<MyViewHolder>() {
+        private var listener: View.OnClickListener? = null
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_recycler_view, parent, false)
+            return MyViewHolder(itemView)
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            holder.positionText.text = position.toString()
+            holder.titleText.text = list[position]
+        }
+        override fun getItemCount(): Int = list.size
+        fun setOnItemClickListener(listener: View.OnClickListener) {
+            this.listener = listener
         }
     }
+
 }
+
+
