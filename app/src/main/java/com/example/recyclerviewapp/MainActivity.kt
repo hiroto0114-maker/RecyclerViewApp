1c646e15e93acf38571dfabe769551979d449bb2
hiroto-murayama <0me6-90y2k-4f0k@au.com>
2021-08-14 21:45:45 +0900

    RecyclerViewApp

--- app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.recyclerviewapp
-
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
 
@@ -31,5 +34,39 @@ class MainActivity : AppCompatActivity() {
         recyclerView = findViewById(R.id.recyclerView)
         recyclerView.adapter = MyRecyclerViewAdapter(pokemonList)
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+    }
+
+    private class MyRecyclerViewAdapter(val list: List<String>) : RecyclerView.Adapter<MyViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_recycler_view, parent, false)
+            itemView.setOnClickListener(ItemClickListener())
+            return MyViewHolder(itemView)
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            holder.positionText.text = position.toString()
+            holder.titleText.text = list[position]
+        }
+
+        override fun getItemCount(): Int = list.size
+
+
+    }
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val positionText: TextView
+        val titleText: TextView
+        init {
+            positionText = itemView.findViewById(R.id.position)
+            titleText = itemView.findViewById(R.id.title)
+        }
+    }
+    class ItemClickListener : View.OnClickListener {
+        override fun onClick(view: View){
+            val pokemonName=view.findViewById<TextView>(R.id.title)
+            val pokemonName1=pokemonName.text.toString()
+            Toast.makeText(this@MainActivity, pokemonName1, Toast.LENGTH_LONG).show()
+        }
     }
 }
