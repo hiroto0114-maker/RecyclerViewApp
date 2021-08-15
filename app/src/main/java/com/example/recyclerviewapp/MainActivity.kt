0e85ac3ac3e8e705b5495858e128bb08c49c9e70
hiroto-murayama <0me6-90y2k-4f0k@au.com>
2021-08-15 22:53:58 +0900

    ボタンクリックしてアプリが強制終了するやつ治った

--- app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
@@ -3,6 +3,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -36,29 +37,51 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         val adapter = MyRecyclerViewAdapter(pokemonList)
         recyclerView.adapter = adapter
-        adapter.setOnItemClickListener(ItemClickListener())
     }
-    inner class ItemClickListener :View.OnClickListener {
+    class ItemClickListener :View.OnClickListener {
         override fun onClick(view: View){
             val pokemonName = view.findViewById<TextView>(R.id.title)
             val pokemonName1 = pokemonName.text.toString()
-            Toast.makeText(this@MainActivity, pokemonName1, Toast.LENGTH_LONG).show()
+            Toast.makeText(view.context, pokemonName1, Toast.LENGTH_LONG).show()
         }
     }
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val positionText: TextView
         val titleText: TextView
         val pokemonButton: TextView
+        val pokemonList = listOf(
+            "ピカチュウ",
+            "カイリュー",
+            "ヤドラン",
+            "ピジョン",
+            "コダック",
+            "コラッタ",
+            "ズバット",
+            "ギャロップ",
+            "サンダース",
+            "メノクラゲ",
+            "パウワウ",
+            "カラカラ",
+            "タマタマ",
+            "ガラガラ",
+            "フシギダネ"
+        )
         init {
             positionText = itemView.findViewById(R.id.position)
             titleText = itemView.findViewById(R.id.title)
             pokemonButton = itemView.findViewById(R.id.pokemon_button)
+            pokemonButton.setOnClickListener{v: View ->
+                val position: Int =adapterPosition
+                val num: Int = position.toInt()
+                Toast.makeText(pokemonButton.context, pokemonList[num], Toast.LENGTH_LONG).show()
+            }
         }
     }
     class MyRecyclerViewAdapter(val list: List<String>) : RecyclerView.Adapter<MyViewHolder>() {
-        private var listener: View.OnClickListener? = null
+        //private var listener: View.OnClickListener? = null
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
             val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_recycler_view, parent, false)
+            itemView.findViewById<Button>(R.id.pokemon_button).setOnClickListener(ItemClickListener())
             return MyViewHolder(itemView)
         }
         override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
@@ -66,9 +89,7 @@ class MainActivity : AppCompatActivity() {
             holder.titleText.text = list[position]
         }
         override fun getItemCount(): Int = list.size
-        fun setOnItemClickListener(listener: View.OnClickListener) {
-            this.listener = listener
-        }
+
     }
 
 }
